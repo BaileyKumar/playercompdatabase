77e018d036af762d4f366015fc88f0aa7f0de72a
BaileyKumar <bailey.kumar@gmail.copm>
2024-12-13 16:56:01 -0500

    updated build and requirments

---
+++ front_end/build/precache-manifest.97fd8186eb07f286da460d5cc353aae4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e45f849c5f481212ed7938eb9bbd56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09428e597f54ce1b63a5",
+    "url": "/static/css/2.92d519d1.chunk.css"
+  },
+  {
+    "revision": "aeabbb47e76f3ac6e3ad",
+    "url": "/static/css/main.b7f46d8e.chunk.css"
+  },
+  {
+    "revision": "09428e597f54ce1b63a5",
+    "url": "/static/js/2.90e5ea9c.chunk.js"
+  },
+  {
+    "revision": "1bc239fba3785fdc2ea10f87e9f905dc",
+    "url": "/static/js/2.90e5ea9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeabbb47e76f3ac6e3ad",
+    "url": "/static/js/main.df8fcbae.chunk.js"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  },
+  {
+    "revision": "062070972fbad0f4fbb3505e6d013a39",
+    "url": "/static/media/primeicons.06207097.woff"
+  },
+  {
+    "revision": "1d854e3ab2c37173804a0d1d1af229ee",
+    "url": "/static/media/primeicons.1d854e3a.svg"
+  },
+  {
+    "revision": "acb19296cb26b15dcf42e13c603217f2",
+    "url": "/static/media/primeicons.acb19296.ttf"
+  },
+  {
+    "revision": "c08d00cac4f2edb5ade360a193fcd08e",
+    "url": "/static/media/primeicons.c08d00ca.eot"
+  },
+  {
+    "revision": "c0929a1e055f2ceb5c889501c24a24e1",
+    "url": "/static/media/primeicons.c0929a1e.woff2"
+  }
+]);
